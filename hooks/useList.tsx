98a76ae5c2189b29fa47ac01ce54b561d0a476a3
Firato <firatofficial@outlook.de>
2024-05-25 02:12:16 +0200

    hopefully fixed?

--- hooks/useList.tsx
+++ hooks/useList.tsx
@@ -1,10 +1,10 @@
 import { db } from "@/firebase";
 import { Movie } from "@/typings";
-import { DocumentData, collection, doc, onSnapshot } from "firebase/firestore";
+import { collection, doc, onSnapshot } from "firebase/firestore";
 import { useEffect, useState } from "react";
 
 function useList(uid: string | undefined) {
-  const [list, setList] = useState<Movie[] | DocumentData>([]);
+  const [list, setList] = useState<Movie[]>([]);
 
   useEffect(() => {
     if (!uid) return;
@@ -13,10 +13,10 @@ function useList(uid: string | undefined) {
       collection(db, "customers", uid, "myList"),
       (snapshot) => {
         setList(
-          snapshot.docs.map((doc) => ({
-            id: doc.id,
-            ...doc.data(),
-          }))
+          snapshot.docs.map((doc) => {
+            const movieData = doc.data() as Movie; // Explicitly cast to Movie
+            return movieData;
+          })
         );
       }
     );
